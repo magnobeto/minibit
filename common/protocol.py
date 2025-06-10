e127661c8177345b225bac2976607093662e3475
roberto.campos <roberto.magno@g.globo>
2025-06-09 22:24:56 -0300

    implementando protocol

---
+++ common/protocol.py
@@ -0,0 +1,8 @@
+class Protocol:
+    @staticmethod
+    def create_message(type, payload):
+        # Serializa mensagem
+
+    @staticmethod
+    def parse_message(data):
+        # Desserializa mensagem

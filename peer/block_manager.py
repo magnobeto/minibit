7cec1e6b7ecb3088b1f25d68ea8fd2b882ded1b1
roberto.campos <roberto.magno@g.globo>
2025-06-09 22:16:36 -0300

    implementando block_manager

---
+++ peer/block_manager.py
@@ -0,0 +1,12 @@
+class BlockManager:
+    def __init__(self):
+        self.blocks = {}  # block_id: data
+
+    def load_initial_blocks(self, total_blocks, initial_subset):
+        # Inicializa com blocos aleatórios
+
+    def has_block(self, block_id):
+        # Retorna se o bloco está presente
+
+    def get_missing_blocks(self):
+        # Lista blocos ausentes

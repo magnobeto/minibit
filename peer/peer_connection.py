590ade9fd7b3c66149548d9ed8663c1775523001
roberto.campos <roberto.magno@g.globo>
2025-06-09 22:20:15 -0300

    implementando connection e unchocke manager

---
+++ peer/peer_connection.py
@@ -0,0 +1,9 @@
+class PeerConnection:
+    def __init__(self, peer_address):
+        self.peer_address = peer_address
+
+    def request_block(self, block_id):
+        # Envia requisição de bloco
+
+    def send_block(self, block_id, data):
+        # Envia bloco para peer
---
+++ peer/unchoke_manager.py
@@ -0,0 +1,10 @@
+class UnchokeManager:
+    def __init__(self):
+        self.fixed_peers = set()
+        self.optimistic_peer = None
+
+    def evaluate_peers(self, known_peers, block_rarity):
+        # Atualiza a lista de peers desbloqueados
+
+    def get_unchoked_peers(self):
+        # Retorna lista de peers que podem trocar blocos
